f14f0e2d2d356563f153ddea3d8ac63bc1130322
Eltensy <tnprost@gmail.com>
2025-09-11 16:37:09 +0300

    2

--- server/db.ts
+++ server/db.ts
@@ -1,16 +1,19 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
+import pg from "pg";
+const { Pool } = pg;
+
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+  throw new Error("DATABASE_URL must be set");
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle({ client: pool, schema });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 5,           // макс. соединений
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+export const db = drizzle(pool, { schema });
