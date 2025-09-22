2eb97536d03239ee9d57adca0608628d59341757
Eltensy <tnprost@gmail.com>
2025-09-22 13:38:05 +0300

    loader 2

--- client/src/pages/not-found.tsx
+++ client/src/pages/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
           </div>
 
           <p className="mt-4 text-sm text-gray-600">
-            Did you forget to add the page to the router?
+            loaderio-9744999ce4ccbe047d786392200c957c
           </p>
         </CardContent>
       </Card>
